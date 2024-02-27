9969f3b6afc09424703c6d8b643681293cd83baa
Eduardo.06 <133702228+EduardoSBM@users.noreply.github.com>
2024-02-27 14:30:06 -0300

    Delete terceirafase/P.O.O directory

--- terceirafase/P.O.O/lista/ex1.py
+++
@@ -1,3 +0,0 @@
-altura = float(input(f"Digite a altura: "))
-base = float(input(f"Digite a base: "))
-print(f" A área do retângulo é {altura * base} ")
--- terceirafase/P.O.O/lista/ex19.py
+++
@@ -1,4 +0,0 @@
-numeros = [10,9,8,7,6,5,3,1,2,4]
-
-numeros.sort()
-print(numeros)
